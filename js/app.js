4043184994887370b46ecb6c5e6f6b620331c469
Matt J. Wiater <matt@brightpixel.com>
2022-12-22 15:34:44 -0800

    styling

--- js/app.js
+++ js/app.js
@@ -1,52 +1,50 @@
+$(document).ready(function () {
+  $(".code-copy-button").each(function (index) {
+    const copyButton = $(this)[0];
+    let codeBoxPos = {}
+    codeBoxPos.top = $(this).siblings().first().position().top;
+    codeBoxPos.right = $(this).siblings().first().position().left + $(this).siblings().first().width();
+    codeBoxPos.bottom = $(this).siblings().first().position().top + $(this).siblings().first().height();
+    codeBoxPos.left = $(this).siblings().first().position().left;
+    if ($(this).parents("pre").length > 0) {
+      codeBoxPos.right = $(this).parents("pre").first().position().left + $(this).parents("pre").first().width() - 10;
+    }
+    $(this).siblings().first().css("padding-right", "30px")
+    $(this).css("position", "absolute");
+    $(this).css("top", codeBoxPos.top + 2);
+    $(this).css("left", codeBoxPos.right + 11);
+    copyButton.addEventListener('click', (event) => {
+      const content = $(this).siblings().first().text();
+      const self = $(this);
+      navigator.clipboard.writeText(content).then(function () {
+        // Promise Success
+        self.removeClass("fa-copy");
+        self.addClass("fa-check");
 
-
-
-$(".code-copy-button").each(function( index ) {
-  const copyButton = $(this)[0];
-  let codeBoxPos = {}
-  codeBoxPos.top = $(this).siblings().first().position().top;
-  codeBoxPos.right = $(this).siblings().first().position().left + $(this).siblings().first().width();
-  codeBoxPos.bottom = $(this).siblings().first().position().top + $(this).siblings().first().height();
-  codeBoxPos.left = $(this).siblings().first().position().left;
-  if($(this).parents("pre").length > 0){
-    codeBoxPos.right = $(this).parents("pre").first().position().left + $(this).parents("pre").first().width()-10;
-  }
-  $(this).siblings().first().css("padding-right", "30px")
-  $(this).css("position", "absolute");
-  $(this).css("top", codeBoxPos.top+2);
-  $(this).css("left", codeBoxPos.right+11);
-  copyButton.addEventListener('click', (event) => {
-    const content = $(this).siblings().first().text();
-    const self = $(this);
-    navigator.clipboard.writeText(content).then(function(){
-      // Promise Success
-      self.removeClass("fa-copy");
-      self.addClass("fa-check");
-      
-      setTimeout(() => {
-        self.removeClass("fa-check");
-        self.addClass("fa-copy");
-      }, 2000)
-    }, function(){
-      // Promise Error
+        setTimeout(() => {
+          self.removeClass("fa-check");
+          self.addClass("fa-copy");
+        }, 2000)
+      }, function () {
+        // Promise Error
+      });
     });
   });
-});
 
-$(".project-section").each(function( index ) {
-  const card = $(this);
-  const link = $(this).find("a")[0];
-  card.css("cursor", "pointer");
+  $(".project-section").each(function (index) {
+    const card = $(this);
+    const link = $(this).find("a")[0];
+    card.css("cursor", "pointer");
 
-  card[0].addEventListener('click', (event) => {
-    link.click()
+    card[0].addEventListener('click', (event) => {
+      link.click()
+    });
+
+    const url = new URL(link.href);
+
+    if (window.location.pathname.slice(1) === url.pathname.slice(1)) {
+      $(this).css("background", "#eff6ef");
+    }
+
   });
-
-  const url = new URL(link.href);
-
-  if(window.location.pathname.slice(1) === url.pathname.slice(1)){
-    console.log($(this))
-    $(this).css("background", "#eff6ef");
-  }
-
 });

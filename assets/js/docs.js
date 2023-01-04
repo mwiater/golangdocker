78b67027e3453e482ffc42cdc51fdbcba869c11c
Matt J. Wiater <matt@brightpixel.com>
2023-01-04 13:57:26 -0800

    nav fixes

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -117,13 +117,6 @@ sidebarLinks.forEach((sidebarLink) => {
 	});
 });
 
-/* ===== Gumshoe SrollSpy ===== */
-/* Ref: https://github.com/cferdinandi/gumshoe  */
-// Initialize Gumshoe
-//var spy = new Gumshoe('#docs-nav a', {
-//	offset: 69 //sticky header height
-//});
-
 
 /* ====== SimpleLightbox Plugin ======= */
 /*  Ref: https://github.com/andreknieriem/simplelightbox */

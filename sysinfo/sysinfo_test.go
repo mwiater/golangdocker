1a3280aeba563cb20d4b34253c0d86b58601e1fe
Matt J. Wiater <matt@brightpixel.com>
2023-10-09 13:13:09 -0700

    updating tests

--- sysinfo/sysinfo_test.go
+++ sysinfo/sysinfo_test.go
@@ -4,12 +4,6 @@ import (
 	"testing"
 
 	"github.com/mwiater/golangdocker/sysinfo"
-	"github.com/shirou/gopsutil/v3/cpu"
-	"github.com/shirou/gopsutil/v3/host"
-	"github.com/shirou/gopsutil/v3/load"
-	"github.com/shirou/gopsutil/v3/mem"
-	"github.com/shirou/gopsutil/v3/net"
-	"github.com/stretchr/testify/assert"
 )
 
 func ExampleTestTZ() {
@@ -25,26 +19,76 @@ func ExampleTestTLS() {
 }
 
 func TestGetMemInfo(t *testing.T) {
-	memStat, _ := sysinfo.GetMemInfo(false)
-	assert.IsType(t, &mem.VirtualMemoryStat{}, memStat)
+	memInfo, err := sysinfo.GetMemInfo(false)
+	if err != nil {
+		t.Fatalf("GetMemInfo() returned an error: %v", err)
+	}
+
+	if memInfo == nil {
+		t.Fatal("GetMemInfo() returned a nil *mem.VirtualMemoryStat object")
+	}
+
+	if memInfo.Total == 0 {
+		t.Error("Expected Total memory to be non-zero")
+	}
 }
 
 func TestGetCPUInfo(t *testing.T) {
-	cpuStat, _ := sysinfo.GetCPUInfo(false)
-	assert.IsType(t, []cpu.InfoStat{}, cpuStat)
+	cpuInfo, err := sysinfo.GetCPUInfo(false)
+	if err != nil {
+		t.Fatalf("GetCPUInfo() returned an error: %v", err)
+	}
+
+	if len(cpuInfo) == 0 {
+		t.Fatal("GetCPUInfo() returned an empty []cpu.InfoStat slice")
+	}
+
+	if cpuInfo[0].ModelName == "" {
+		t.Error("Expected the first CPU's ModelName to be non-empty")
+	}
 }
 
 func TestGetHostInfo(t *testing.T) {
-	hostStat, _ := sysinfo.GetHostInfo(false)
-	assert.IsType(t, &host.InfoStat{}, hostStat)
+	hostInfo, err := sysinfo.GetHostInfo(false)
+	if err != nil {
+		t.Fatalf("GetHostInfo() returned an error: %v", err)
+	}
+
+	if hostInfo == nil {
+		t.Fatal("GetHostInfo() returned a nil *host.InfoStat object")
+	}
+
+	if hostInfo.Hostname == "" {
+		t.Error("Expected Hostname to be non-empty")
+	}
 }
 
 func TestGetNetInfo(t *testing.T) {
-	netStat, _ := sysinfo.GetNetInfo(false)
-	assert.IsType(t, []net.InterfaceStat{}, netStat)
+	netInfo, err := sysinfo.GetNetInfo(false)
+	if err != nil {
+		t.Fatalf("GetNetInfo() returned an error: %v", err)
+	}
+
+	if len(netInfo) == 0 {
+		t.Fatal("GetNetInfo() returned an empty []net.InterfaceStat slice")
+	}
+
+	if netInfo[0].Name == "" {
+		t.Error("Expected the first network interface's Name to be non-empty")
+	}
 }
 
 func TestGetLoadInfo(t *testing.T) {
-	loadStat, _ := sysinfo.GetLoadInfo(false)
-	assert.IsType(t, &load.AvgStat{}, loadStat)
+	loadInfo, err := sysinfo.GetLoadInfo(false)
+	if err != nil {
+		t.Fatalf("GetLoadInfo() returned an error: %v", err)
+	}
+
+	if loadInfo == nil {
+		t.Fatal("GetLoadInfo() returned a nil *load.AvgStat object")
+	}
+
+	if loadInfo.Load1 < 0 {
+		t.Error("Expected Load1 (1-minute load average) to be non-negative")
+	}
 }
